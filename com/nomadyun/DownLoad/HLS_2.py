f4def4a7ab399c2579a9efd8f7cb7e477b598505
nomadyun <nomadyun@gmail.com>
2015-09-21 12:44:09 +0800

    Clear

---
+++ com/nomadyun/DownLoad/HLS_2.py
@@ -0,0 +1,105 @@
+#-*- coding:utf-8 -*-
+'''
+Created on 2015-04-01
+
+@author: huang_yun
+'''
+
+import re,os,threading
+import urllib,urllib2
+
+p_url = re.compile(r'(http(s*)://.+/)(.*.m3u8)')
+p_m3u8 = re.compile('.*.m3u8')
+p_ts = re.compile('.*.ts')
+p_key = re.compile('#EXT-X-KEY:METHOD=AES-128,URI="(\d*.key)"')
+
+m3u8_list = []
+ts_list = []
+key_list = []
+
+down_path = 'F:/HLS_Download/'
+baseUrl = 'http://qthttp.apple.com.edgesuite.net/1010qwoeiuryfg/1240_vod.m3u8'
+
+match = p_url.match(baseUrl)
+if match:
+    #root of the url
+    urlRoot = match.group(1)
+    print urlRoot
+else:
+    print 'Please check the url!'
+
+#prepare download directory
+if not os.path.exists(down_path):
+    os.makedirs(down_path)
+os.chdir(down_path)
+print 'Files will be downloaded to:' + '' + os.getcwd()
+
+def M3U8Paser(URI):
+    req = urllib2.Request(URI)
+
+    try: 
+        response = urllib2.urlopen(req, timeout=10)
+
+    except urllib2.URLError, e:        
+        if hasattr(e, 'code'):            
+            print 'The server couldn\'t fulfill the request.'           
+            print 'Error code: ', e.code    
+      
+        elif hasattr(e, 'reason'):           
+            print 'We failed to reach a server.'           
+            print 'Reason: ', e.reason    
+
+    else:    
+        content = response.readlines()
+        for line in content:
+            m_m3u8 = p_m3u8.search(line)
+            m_ts = p_ts.search(line)
+            m_key = p_key.search(line)
+            #sub url is m3u8
+            if m_m3u8:
+                m3u8_list.append(m_m3u8.group(0))
+                sub_m3u8 = m_m3u8.group(0)
+                sub_m3u8_url = urlRoot + sub_m3u8
+                # 创建新线程
+                t = downloader(sub_m3u8_url,sub_m3u8)
+                # 添加线程到线程列表
+                threads.append(t)
+                # 开启新线程
+                t.start()
+                #recursive m3u8 url
+                M3U8Paser(sub_m3u8_url)
+            #sub url is ts
+            elif m_ts:
+                ts_list.append(m_ts.group(0))
+                ts = m_ts.group(0)
+                ts_url = urlRoot + ts
+                t = downloader(ts_url,ts)
+                threads.append(t)
+                t.start()
+            #sub url is key        
+            elif m_key:
+                key_list.append(m_key.group(1))
+                key = m_key.group(1)
+                key_url = urlRoot + key
+                t = downloader(key_url,key)
+                threads.append(t)
+                t.start()    
+        print "Download completed!" 
+              
+class downloader(threading.Thread):
+        def __init__(self, url, name):
+            threading.Thread.__init__(self)
+            self.url=url
+            self.name=name
+
+        def run(self):
+            print 'downloading %s' % self.url
+            urllib.urlretrieve(self.url, self.name)
+
+threads=[]
+
+if __name__ == '__main__':
+    M3U8Paser(baseUrl)
+    for t in threads:
+        t.join()
+    
--- com/nomadyun/DownLoad/HLS_Iframe.py
+++ com/nomadyun/DownLoad/HLS_Iframe.py
@@ -8,38 +8,49 @@ Test
 import re,os
 import urllib,urllib2
 #URI="01-iframe.m3u8"
-p_m3u8 = re.compile('.*.m3u8')
+p_m3u8 = re.compile('0\d.m3u8')
 #stream-01-31/247.ts
 p_ts = re.compile(r'(.*)/(.*.ts)')
 
-down_path = 'J:/HLS_Download'
-baseUrl = 'http://213.65.40.18:8090/session/8b57ea72-5de9-11e5-ae49-984be10b109c/wp5abc/\
-c_116_hls_fuel_tv_abf4730f4fedc76717a6298e907e4019/index.m3u8?token=f9238d3442d7ee8efac65e701bd702fa_1442641333'
+down_path = 'F:/HLS_Download/Test'
+baseUrl = 'http://213.65.40.18:8090/session/b0bac46a-6005-11e5-9cc6-984be10b109c/wp5abc/\
+c_116_hls_fuel_tv_abf4730f4fedc76717a6298e907e4019/index.m3u8?token=9999630a0c3d1256b0288721d2ecad65_1442873323'
 
-urlRoot = 'http://213.65.40.18:8090/session/8b57ea72-5de9-11e5-ae49-984be10b109c/wp5abc/\
+urlRoot = 'http://213.65.40.18:8090/session/b0bac46a-6005-11e5-9cc6-984be10b109c/wp5abc/\
 c_116_hls_fuel_tv_abf4730f4fedc76717a6298e907e4019/'
 
+m3u8_list = []
+ts_list = []
+
 def M3U8Paser(URI):
-    m3u8_list = []
-    ts_list = []
-    content = urllib2.urlopen(URI)   
-    res = content.readlines()
-   
-    for line in res:
-        m_m3u8 = p_m3u8.search(line)
-        print line
-        m_ts = p_ts.search(line)
-        
-        if m_m3u8:
-            m3u8_list.append(m_m3u8.group(0))
-            sub_m3u8 = m_m3u8.group(0)
-            sub_m3u8_url = urlRoot + sub_m3u8
-            M3U8Paser(sub_m3u8_url)
-        elif m_ts:
-            ts_list.append(m_ts.group(0))
-    print m3u8_list
-    return ts_list   
-      
+    req = urllib2.Request(URI)
+    try: 
+        response = urllib2.urlopen(req, timeout=10)
+    except urllib2.URLError, e:        
+        if hasattr(e, 'code'):            
+            print 'The server couldn\'t fulfill the request.'           
+            print 'Error code: ', e.code          
+        elif hasattr(e, 'reason'):           
+            print 'We failed to reach a server.'           
+            print 'Reason: ', e.reason    
+    else:    
+        content = response.readlines()
+        for line in content:
+            m_m3u8 = p_m3u8.search(line)
+            m_ts = p_ts.search(line)
+                        
+            if m_m3u8:
+                m3u8_list.append(m_m3u8.group(0))
+                #print line
+                sub_m3u8 = m_m3u8.group(0)
+                sub_m3u8_url = urlRoot + sub_m3u8
+                M3U8Paser(sub_m3u8_url)
+                    
+            elif m_ts:
+                ts_list.append(m_ts.group(0))
+                #print ts_list
+
+
 def download_ts(URI):
     '''
     if not os.path.exists(down_path):
@@ -63,3 +74,4 @@ def download_ts(URI):
 if __name__ == '__main__':
     #download_ts(baseUrl)
     M3U8Paser(baseUrl)
+    print m3u8_list
