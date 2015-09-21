1071f0a59b2d834291043c85092e7f93b7a9bcfb
nomadyun <nomadyun@gmail.com>
2015-09-21 23:14:53 +0800

    add threads

--- com/nomadyun/DownLoad/HLS_Iframe.py
+++ com/nomadyun/DownLoad/HLS_Iframe.py
@@ -5,14 +5,16 @@ Test
 @author: huang_yun
 '''
 
-import re,os
+import re,os,threading
+#import socket
 import urllib,urllib2
+#socket.setdefaulttimeout(5.0)
 #URI="01-iframe.m3u8"
 p_m3u8 = re.compile('0\d.m3u8')
 #stream-01-31/247.ts
 p_ts = re.compile(r'(.*)/(.*.ts)')
 
-down_path = 'F:/HLS_Download/Test'
+down_path = 'J:/HLS_Download/Test'
 baseUrl = 'http://213.65.40.18:8090/session/b0bac46a-6005-11e5-9cc6-984be10b109c/wp5abc/\
 c_116_hls_fuel_tv_abf4730f4fedc76717a6298e907e4019/index.m3u8?token=9999630a0c3d1256b0288721d2ecad65_1442873323'
 
@@ -49,29 +51,35 @@ def M3U8Paser(URI):
             elif m_ts:
                 ts_list.append(m_ts.group(0))
                 #print ts_list
+                ts = m_ts.group(0)
+                sub_dir = m_ts.group(1)                
+                ts_name = m_ts.group(2)                
+                ts_url = urlRoot + ts
+                dir_name = os.path.join(down_path,sub_dir)
+                if not os.path.exists(dir_name):
+                    os.makedirs(dir_name)
+                os.chdir(dir_name)
+                #print 'Files will be downloaded to:' + '' + os.getcwd()        
+                #print 'Downloading:' + ts             
+                t = downloader(ts_url,ts_name)
+                threads.append(t)
+                t.start()
 
+class downloader(threading.Thread):
+        def __init__(self, url, name):
+            threading.Thread.__init__(self)
+            self.url=url
+            self.name=name
 
-def download_ts(URI):
-    '''
-    if not os.path.exists(down_path):
-        os.makedirs(down_path)
-    os.chdir(down_path)
-    print 'Files will be downloaded to:' + '' + os.getcwd()
-    '''
-    TS_Files = M3U8Paser(URI)
-    for ts_file in TS_Files:
-        m_ts = p_ts.search(ts_file)
-        sub_dir = m_ts.group(1)
-        ts_name = m_ts.group(2)
-        if not os.path.exists(down_path/sub_dir):
-            os.makedirs(down_path/sub_dir)
-        os.chdir(down_path/sub_dir)
-        print 'Files will be downloaded to:' + '' + os.getcwd()        
-        tsURL = urlRoot + ts_file
-        print 'Downloading:' + ts_file
-        urllib.urlretrieve(tsURL,ts_name)
+        def run(self):
+            print 'downloading %s' % self.url
+            urllib.urlretrieve(self.url, self.name)
+
+threads=[]
 
 if __name__ == '__main__':
     #download_ts(baseUrl)
     M3U8Paser(baseUrl)
-    print m3u8_list
+    #print m3u8_list
+    for t in threads:
+        t.join()   
